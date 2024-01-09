3a553b822c705b66fa8b56ee10fb553c875657b6
Asep Abdaz <developer@xabdaz.tech>
2024-01-09 09:11:22 +0700

    initial commit

---
+++ okhomeapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  okhomeapp
+//
+//  Created by xabdaz on 09/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
